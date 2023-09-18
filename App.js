e4ecd8c16f84e04e6f7d25b2a2f3c4fac8a83dc8
Harsh Chandwani <harshchandwani50028@gmail.com>
2023-09-18 13:29:48 +0530

    feat: Implement header component
    - Created header component
    - Added navigation links and logo

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import { GardenCart } from 'tabler-icons-react';
 
-const Title = () => (
+// - Header
 
-    <h1> Hey there this is a title</h1>
-);
+//   - Logo
+//   - Nav Items (Home, About, Cart)
+const Header = () => {
+    return (
+        <div className="header">
+            <div>
+                <img className="logo" src="https://img.freepik.com/free-vector/delivery-logo-template_23-2147880262.jpg?w=826&t=st=1695013308~exp=1695013908~hmac=a278336d91250224cc3a981983f766e16c0ef3168f73090acc2cc7f609e857e9"></img>
+            </div>
 
-const HeadingComponent = () => (
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact uS</li>
+                    <li>Cart</li>
 
-    <div className="heading" id="container">
-        <h1>Hello world from React Functional Component</h1>
-        <Title></Title>
+                </ul>
+            </div>
+        </div>
+    )
+};
 
-    </div>
-
-);
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
--- Readme.md
+++ Readme.md
@@ -1,1 +1,22 @@
-#Hello There
+# Food Ordering App
+
+# Here is the Complete structure how we build it:
+
+We have divided the web app into 3 parts
+
+- Header
+
+  - Logo
+  - Nav Items (Home, About, Cart)
+
+- Body
+
+  - Search
+  - Restaurant Container
+    - Restaurant Card
+
+- Footer
+  - Copyright
+  - Links
+  - Address
+  - Contact

32f7c2a6953ed20bdae09e502e160388f7e8dae2
Harsh Chandwani <harshchandwani50028@gmail.com>
2023-11-11 18:22:57 +0530

    Corrected Deployment Errors

--- src/components/Body.js
+++ src/components/Body.js
@@ -22,7 +22,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.694490&lng=76.665092&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.694490&lng=76.665092&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
